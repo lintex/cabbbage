4ef215461bf0f2ec8fbc7cc9b408f87b3041ba8b
lintex <lintex@163.com>
2024-04-09 16:10:18 +0800

    MacOS 配置成功

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -7,6 +7,14 @@ class AboutPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextStyle greyText = const TextStyle(
+      color: Colors.black54,
+      fontSize: 12,
+    );
+    TextStyle underlineText = const TextStyle(
+        color: Colors.black54,
+        fontSize: 12,
+        decoration: TextDecoration.underline);
     return Scaffold(
         appBar: const MyAppBar(title: "关于"),
         body: Center(
@@ -14,31 +22,75 @@ class AboutPage extends StatelessWidget {
             padding: const EdgeInsets.fromLTRB(15, 50, 15, 30),
             child: Column(
               children: [
-                Image.asset(
-                  "assets/images/logo.png",
-                  width: 70,
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                const Text("一个又卷又菜的APP",
-                    style: TextStyle(fontFamily: '字语相思体', fontSize: 28)),
-                const SizedBox(height: 15),
-                const Text(
-                  "1.0.1",
-                  style: TextStyle(
-                    color: Colors.black45,
+                Expanded(
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 95,
+                      ),
+                      Image.asset(
+                        "assets/images/logo.png",
+                        width: 70,
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      const Text("再菜也要用心卷",
+                          style: TextStyle(fontFamily: '字语相思体', fontSize: 28)),
+                      const SizedBox(height: 15),
+                      const Text(
+                        "卷心菜 0.0.1",
+                        style: TextStyle(
+                          color: Colors.black45,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      MyButton(
+                          text: "检查更新",
+                          color: Colors.green[300]!,
+                          onPressed: () {}),
+                      // Image.asset(
+                      //   "assets/images/about.jpg",
+                      //   width: 200,
+                      // ),
+                    ],
                   ),
                 ),
-                const SizedBox(
-                  height: 25,
-                ),
-                MyButton(
-                    text: "检查更新", color: Colors.green[300]!, onPressed: () {})
-                // Image.asset(
-                //   "assets/images/about.jpg",
-                //   width: 200,
-                // ),
+                Column(
+                  children: [
+                    Text(
+                      "小西软件 版权所有",
+                      style: greyText,
+                    ),
+                    Text(
+                      "Copyright ©️ 2024 SmallWest Software. All rights reserved.",
+                      style: greyText,
+                    ),
+                    Text(
+                      "ICP备案号：鄂B2-20240059-1745A",
+                      style: greyText,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "用户协议",
+                          style: underlineText,
+                        ),
+                        Text(
+                          " · ",
+                          style: greyText,
+                        ),
+                        Text(
+                          "隐私政策",
+                          style: underlineText,
+                        ),
+                      ],
+                    ),
+                  ],
+                )
               ],
             ),
           ),
