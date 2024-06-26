8c7682c35e5e1865f561d5a7c749cae672949ddb
lintex <lintex@163.com>
2024-06-26 16:55:26 +0800

    - 🆕「仪表盘」马拉松助手模块，添加CircularPercentIndicator显示倒计时天数 - 🐞「马拉松助手」过期天数显示为负数 - 🆕「马拉松助手」列表页Tile底部添加LinearPercentIndicator

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:cabbbage/components/my_bottom_sheet.dart';
 import 'package:cabbbage/components/my_card_content.dart';
 import 'package:cabbbage/components/my_circle_tool_button.dart';
@@ -6,12 +8,14 @@ import 'package:clipboard/clipboard.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:cabbbage/components/my_dashboard_tile.dart';
 import 'package:cabbbage/components/my_drawer.dart';
 import 'package:cabbbage/models/database.dart';
 import 'package:cabbbage/tools/tools.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
@@ -33,7 +37,8 @@ class DashboardPage extends StatelessWidget {
       19.5,
       89.2,
     ];
-
+    // 马拉松倒计时，用于CircularPercentIndicator显示数据
+    List<int> marathonStillDays = [];
     return Theme(
       // 这段代码是为了去除persistentFooterButtons顶部一条分割线
       data: Theme.of(context).copyWith(
@@ -138,7 +143,7 @@ class DashboardPage extends StatelessWidget {
                           // 改变提示框背景，要用这种奇葩的写法
                           touchTooltipData: BarTouchTooltipData(
                               getTooltipColor: (group) =>
-                                  Colors.lightGreen.shade200)),
+                                  Colors.lightGreen.shade100)),
 
                       // 条形图的数据
                       // map遍历获取List的索引值，只能用List.asMap().keys.map这种奇怪的写法
@@ -159,15 +164,86 @@ class DashboardPage extends StatelessWidget {
                     swapAnimationCurve: Curves.easeInOut,
                   ),
                 )),
+                // 马拉松倒计时模块
                 GestureDetector(
                   onTap: () => Get.toNamed('/marathon'),
                   child: MyDashboardTile(
                     child: Padding(
                       padding: const EdgeInsets.fromLTRB(20, 0, 15, 0),
-                      child: Column(
-                          children: db.stillMarathons
-                              .map((m) => MyDashboardMarathonTile(marathon: m))
-                              .toList()),
+                      // 使用Stack堆叠一个percent指示器
+                      child: Stack(
+                        children: [
+                          Column(
+                              children: db.stillMarathons.map((m) {
+                            marathonStillDays.add(
+                              int.parse(Tools.diffDays(m.time!)),
+                            );
+                            return MyDashboardMarathonTile(marathon: m);
+                          }).toList()),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: SizedBox(
+                              height: 100,
+                              width: 100,
+                              child: Stack(
+                                children: [
+                                  Align(
+                                    alignment: Alignment.center,
+                                    child: CircularPercentIndicator(
+                                      radius: 45.0,
+                                      animation: true,
+                                      animationDuration: 1200,
+                                      lineWidth: 8.0,
+                                      percent: marathonStillDays[0] / 100,
+                                      center: Text(
+                                          "${marathonStillDays.reduce(min)}天"),
+                                      progressColor: Colors.lightGreen.shade500,
+                                      backgroundColor:
+                                          Colors.lightGreen.shade200,
+                                      circularStrokeCap:
+                                          CircularStrokeCap.round,
+                                    ),
+                                  ),
+                                  Align(
+                                    alignment: Alignment.center,
+                                    child: CircularPercentIndicator(
+                                      radius: 36.5,
+                                      animation: true,
+                                      animationDuration: 1200,
+                                      lineWidth: 8.0,
+                                      percent: marathonStillDays[1] / 100,
+                                      center: Text(
+                                          "${marathonStillDays.reduce(min)}天"),
+                                      progressColor: Colors.lightGreen.shade600,
+                                      backgroundColor:
+                                          Colors.lightGreen.shade200,
+                                      circularStrokeCap:
+                                          CircularStrokeCap.round,
+                                    ),
+                                  ),
+                                  Align(
+                                    alignment: Alignment.center,
+                                    child: CircularPercentIndicator(
+                                      radius: 28.0,
+                                      animation: true,
+                                      animationDuration: 1200,
+                                      lineWidth: 8.0,
+                                      percent: marathonStillDays[2] / 100,
+                                      center: Text(
+                                          "${marathonStillDays.reduce(min)}天"),
+                                      progressColor: Colors.lightGreen.shade700,
+                                      backgroundColor:
+                                          Colors.lightGreen.shade200,
+                                      circularStrokeCap:
+                                          CircularStrokeCap.round,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
--- lib/pages/marathonPage/my_marathon_list_tile.dart
+++ lib/pages/marathonPage/my_marathon_list_tile.dart
@@ -4,6 +4,7 @@ import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:get/get.dart';
 import 'package:cabbbage/models/marathon.dart';
 import 'package:cabbbage/models/database.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class MyMarathonListTile extends StatelessWidget {
   MyMarathonListTile({super.key, required this.marathon});
@@ -22,6 +23,9 @@ class MyMarathonListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // 还剩多少天
+    int stillDays = marathon.time!.difference(DateTime.now()).inDays.abs();
+    int stillMinutes = marathon.time!.difference(DateTime.now()).inMinutes;
     return Container(
       margin: const EdgeInsets.fromLTRB(18, 18, 18, 0),
       child: Slidable(
@@ -43,76 +47,87 @@ class MyMarathonListTile extends StatelessWidget {
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(12),
               //根据天数改变背景色
-              color: DateTime.parse(marathon.time.toString())
-                          .difference(DateTime.now())
-                          .inMinutes <
-                      0
+              color: stillMinutes < 0
                   ? Theme.of(context).colorScheme.primary
                   : Theme.of(context).colorScheme.primaryContainer),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  // 显示马拉松名称
-                  Text(
-                    marathon.name,
-                    style: const TextStyle(
-                        fontSize: 16,
-                        fontFamily: "霞鹜文楷",
-                        fontWeight: FontWeight.bold),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // 显示马拉松名称
+                      Text(
+                        marathon.name,
+                        style: const TextStyle(
+                            fontSize: 16,
+                            fontFamily: "霞鹜文楷",
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      // 显示马拉松举办时间和地点
+                      Text(
+                        getDateAndLocation(marathon.time!,
+                            marathon.start == '' ? '' : ' | ${marathon.start}'),
+                        style: TextStyle(
+                            fontSize: 12,
+                            fontFamily: "霞鹜文楷",
+                            color:
+                                Theme.of(context).colorScheme.inversePrimary),
+                      ),
+                    ],
                   ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  // 显示马拉松举办时间和地点
-                  Text(
-                    getDateAndLocation(marathon.time!,
-                        marathon.start == '' ? '' : ' | ${marathon.start}'),
-                    style: TextStyle(
-                        fontSize: 12,
-                        fontFamily: "霞鹜文楷",
-                        color: Theme.of(context).colorScheme.inversePrimary),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text(
+                        stillMinutes < 0 ? "过了" : "还有",
+                        style: const TextStyle(fontSize: 9, color: Colors.grey),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        stillDays == 0
+                            ? marathon.time!
+                                .difference(DateTime.now())
+                                .inHours
+                                .abs()
+                                .toString()
+                            : Tools.diffDays(marathon.time!),
+                        style: const TextStyle(fontSize: 30),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        stillDays == 0 ? "小时" : "天",
+                        style: const TextStyle(fontSize: 9, color: Colors.grey),
+                      ),
+                    ],
                   ),
                 ],
               ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.baseline,
-                textBaseline: TextBaseline.alphabetic,
-                children: [
-                  Text(
-                    DateTime.parse(marathon.time.toString())
-                                .difference(DateTime.now())
-                                .inMinutes <
-                            0
-                        ? "过了"
-                        : "还有",
-                    style: const TextStyle(fontSize: 9, color: Colors.grey),
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  Text(
-                    marathon.time!.difference(DateTime.now()).inDays.abs() == 0
-                        ? marathon.time!
-                            .difference(DateTime.now())
-                            .inHours
-                            .abs()
-                            .toString()
-                        : Tools.diffDays(marathon.time!),
-                    style: const TextStyle(fontSize: 30),
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  Text(
-                    marathon.time!.difference(DateTime.now()).inDays.abs() == 0
-                        ? "小时"
-                        : "天",
-                    style: const TextStyle(fontSize: 9, color: Colors.grey),
-                  ),
-                ],
+              Container(
+                margin: const EdgeInsets.only(top: 8),
+                width: double.infinity,
+                child: LinearPercentIndicator(
+                  lineHeight: 6.0,
+                  percent: stillMinutes < 0
+                      ? 0
+                      : (stillDays < 100 ? (stillDays / 100) : 1),
+                  animation: true,
+                  isRTL: true, // 从右向左调转方向
+                  padding: const EdgeInsets.all(0),
+                  backgroundColor: Colors.grey.shade200,
+                  progressColor: Colors.lightGreen.shade300,
+                  barRadius: const Radius.circular(5),
+                ),
               ),
             ],
           ),
